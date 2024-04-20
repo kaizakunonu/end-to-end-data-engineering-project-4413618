e5a08b08c53a737d05b8982590f9c38da154e70d
Kaiza Kunonu <kunonu@proton.me>
2024-04-20 17:40:23 +0200

    My First commit

---
+++ dbt_transformation/models/staging/stg_orders.sql
@@ -0,0 +1,10 @@
+
+# dbt_transformation/models/staging/stg_orders.sql
+
+select
+    order_id,
+    customer_id,
+    status as order_status,
+    order_approved_at,
+    order_delivered_at,
+from {{ source("raw_dataset", "orders") }}
